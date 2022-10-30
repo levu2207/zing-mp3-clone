3d3d6849b0a5ed9874a7b4d430dfe6aa73046a7d
levu2207 <levu2207@gmail.com>
2022-10-30 22:01:41 +0700

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,24 +23,24 @@ import './home.css'
 
 const Home = () => {
   const dispatch = useDispatch()
-  // useEffect(() => {
-  //   mp3Service.getHome().then((res) => {
-  //     dispatch(loadBanner(res.data.items[0]))
-  //     dispatch(loadNewRelease(res.data.items[3]))
-  //     dispatch(loadDayDes(res.data.items[4]))
-  //     dispatch(loadTopSinger(res.data.items[5]))
-  //     dispatch(loadNewMusicEveryDay(res.data.items[6]))
-  //     dispatch(loadZingChart(res.data.items[7]))
-  //     dispatch(loadWeekChart(res.data.items[8]))
-  //     dispatch(loadArtistSpotlight(res.data.items[9]))
-  //     dispatch(loadTop100(res.data.items[10]))
-  //     dispatch(loadNewSong(res.data.items[11]))
-  //     dispatch(loadAfterNewSong(res.data.items[12]))
-  //     dispatch(loadAlbumArtist(res.data.items[14]))
-  //     dispatch(loadRadio(res.data.items[15]))
-  //     dispatch(loadEvent(res.data.items[16]))
-  //   })
-  // }, [dispatch])
+  useEffect(() => {
+    mp3Service.getHome().then((res) => {
+      dispatch(loadBanner(res.data.items[0]))
+      dispatch(loadNewRelease(res.data.items[3]))
+      dispatch(loadDayDes(res.data.items[4]))
+      dispatch(loadTopSinger(res.data.items[5]))
+      dispatch(loadNewMusicEveryDay(res.data.items[6]))
+      dispatch(loadZingChart(res.data.items[7]))
+      dispatch(loadWeekChart(res.data.items[8]))
+      dispatch(loadArtistSpotlight(res.data.items[9]))
+      dispatch(loadTop100(res.data.items[10]))
+      dispatch(loadNewSong(res.data.items[11]))
+      dispatch(loadAfterNewSong(res.data.items[12]))
+      dispatch(loadAlbumArtist(res.data.items[14]))
+      dispatch(loadRadio(res.data.items[15]))
+      dispatch(loadEvent(res.data.items[16]))
+    })
+  }, [dispatch])
 
   const banner = useSelector((state) => state.home.banner)
 
