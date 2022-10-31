84ee0bb1720ed5c5843216e9dc50a53c209dca63
levu2207 <levu2207@gmail.com>
2022-10-31 11:39:13 +0700

    fix

--- src/containers/Player/PlayerItem.js
+++ src/containers/Player/PlayerItem.js
@@ -7,27 +7,22 @@ import truncateText from '../../utils/truncateText'
 const PlayerItem = () => {
   const song = useSelector((state) => state.play.playItem)
   console.log(song)
+  if (song === {}) return
   return (
-    <>
-      {song !== {} ? (
-        <div className="player-item flex items-center">
-          <div className="player-item-img w-[64px] h-[64px] mr-2.5 relative">
-            <img className="w-full h-full rounded" src={song.thumbnail} alt="song" />
-          </div>
+    <div className="player-item flex items-center">
+      <div className="player-item-img w-[64px] h-[64px] mr-2.5 relative">
+        <img className="w-full h-full rounded" src={song.thumbnail} alt="song" />
+      </div>
 
-          <div className="song-info flex flex-col justify-center">
-            <p className="mb-1 text-white font-normal">{truncateText(song.title, 24)}</p>
-            <p className="mb-1 text-xs text-[#FFFFFF80]">{truncateText(song.artistsNames, 24)}</p>
-          </div>
+      <div className="song-info flex flex-col justify-center">
+        <p className="mb-1 text-white font-normal">{truncateText(song.title, 24)}</p>
+        <p className="mb-1 text-xs text-[#FFFFFF80]">{truncateText(song.artistsNames, 24)}</p>
+      </div>
 
-          <AddLibrary />
+      <AddLibrary />
 
-          <Option />
-        </div>
-      ) : (
-        ''
-      )}
-    </>
+      <Option />
+    </div>
   )
 }
 
