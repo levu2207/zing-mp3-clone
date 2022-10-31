ac67a07e041df9c27ddbfc7e087076b38d405c8b
levu2207 <levu2207@gmail.com>
2022-10-31 11:24:24 +0700

    fix

--- src/components/NewRelease/NewRelease.js
+++ src/components/NewRelease/NewRelease.js
@@ -8,25 +8,26 @@ import { useSelector } from 'react-redux'
 
 const NewRelease = () => {
   const newRelease = useSelector((state) => state.home.newRelease)
+  const newReleaseList = newRelease.items
+  const keys = Object.keys(newReleaseList)
 
-  const [newReleaseList] = newRelease.items
-  const songs = newReleaseList.song
-  const albums = newReleaseList.album
+  const tab1 = newReleaseList[keys[0]]
+  const tab2 = newReleaseList[keys[1]]
 
   const [currentTab, setCurrentTab] = useState('1')
 
   const tabs = [
     {
       id: '1',
-      tabTitle: 'BÀI HÁT',
+      tabTitle: keys[0] === 'vPop' || keys[0] === 'others' ? 'VIỆT NAM' : 'BÀI HÁT',
       value: '',
-      name: 'song',
+      name: 'tab1',
     },
     {
       id: '2',
-      tabTitle: 'ALBUM',
+      tabTitle: (keys[0] === 'vPop') | (keys[0] === 'others') ? 'QUỐC TẾ' : 'ALBUM',
       value: '',
-      name: 'album',
+      name: 'tab2',
     },
   ]
 
@@ -39,12 +40,21 @@ const NewRelease = () => {
       <div className="new-release-title">
         <p className="text-white text-xl font-bold mb-5">{newRelease.title}</p>
       </div>
-      <div className="new-release-content">
-        <Tabs onClick={(e) => handleTabClick(e)} currentTab={currentTab} tabs={tabs} />
-        <div className="content-info mt-4">
-          {currentTab === '1' ? <Song songs={songs} /> : <Album albums={albums} />}
+      {keys[0] === 'song' || keys[0] === 'album' ? (
+        <div className="new-release-content">
+          <Tabs onClick={(e) => handleTabClick(e)} currentTab={currentTab} tabs={tabs} />
+          <div className="content-info mt-4">
+            {currentTab === '1' ? <Song songs={tab1} /> : <Album albums={tab2} />}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="new-release-content">
+          <Tabs onClick={(e) => handleTabClick(e)} currentTab={currentTab} tabs={tabs} />
+          <div className="content-info mt-4">
+            {currentTab === '1' ? <Song songs={tab1} /> : <Song songs={tab2} />}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/containers/Player/PlayerItem.js
+++ src/containers/Player/PlayerItem.js
@@ -8,20 +8,26 @@ const PlayerItem = () => {
   const song = useSelector((state) => state.play.playItem)
 
   return (
-    <div className="player-item flex items-center">
-      <div className="player-item-img w-[64px] h-[64px] mr-2.5 relative">
-        <img className="w-full h-full rounded" src={song.thumbnail} alt="song" />
-      </div>
+    <>
+      {song ? (
+        <div className="player-item flex items-center">
+          <div className="player-item-img w-[64px] h-[64px] mr-2.5 relative">
+            <img className="w-full h-full rounded" src={song.thumbnail} alt="song" />
+          </div>
 
-      <div className="song-info flex flex-col justify-center">
-        <p className="mb-1 text-white font-normal">{truncateText(song.title, 24)}</p>
-        <p className="mb-1 text-xs text-[#FFFFFF80]">{truncateText(song.artistsNames, 24)}</p>
-      </div>
+          <div className="song-info flex flex-col justify-center">
+            <p className="mb-1 text-white font-normal">{truncateText(song.title, 24)}</p>
+            <p className="mb-1 text-xs text-[#FFFFFF80]">{truncateText(song.artistsNames, 24)}</p>
+          </div>
 
-      <AddLibrary />
+          <AddLibrary />
 
-      <Option />
-    </div>
+          <Option />
+        </div>
+      ) : (
+        ''
+      )}
+    </>
   )
 }
 
