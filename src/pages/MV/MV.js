384ad20f8d10972c0639df21444c1132458e6ba7
levu2207 <levu2207@gmail.com>
2022-11-22 11:19:59 +0700

    fix video modal

--- src/pages/MV/MV.js
+++ src/pages/MV/MV.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import DetailVideo from './DetailVideo'
 import './mv.css'
 
 const MV = () => {
   const navigate = useNavigate()
+  const currentVideo = useSelector((state) => state.video.currentVideo)
 
   useEffect(() => {
     navigate('/the-loai-video/Viet-Nam')
@@ -56,7 +58,7 @@ const MV = () => {
         <Outlet />
       </div>
 
-      <DetailVideo />
+      {Object.keys(currentVideo).length > 0 ? <DetailVideo /> : ''}
     </>
   )
 }
